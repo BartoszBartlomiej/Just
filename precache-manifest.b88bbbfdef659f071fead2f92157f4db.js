b3b62ecf3f7b3d44b3796268a2e094be34247859
Bartosz <bartosz.lewicki@gmail.com>
2019-03-11 15:06:42 +0100

    Updates

---
+++ precache-manifest.b88bbbfdef659f071fead2f92157f4db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a137d06b2347898d6440",
+    "url": "/JustTrip/static/css/main.39994208.chunk.css"
+  },
+  {
+    "revision": "a137d06b2347898d6440",
+    "url": "/JustTrip/static/js/main.a137d06b.chunk.js"
+  },
+  {
+    "revision": "3b4e13777aecec707710",
+    "url": "/JustTrip/static/js/runtime~main.3b4e1377.js"
+  },
+  {
+    "revision": "78ee6262be31967c7d45",
+    "url": "/JustTrip/static/js/2.78ee6262.chunk.js"
+  },
+  {
+    "revision": "70beebc45809d655726c1955ddd2bc25",
+    "url": "/JustTrip/index.html"
+  }
+];
