033e2fb691f421caafa725f3ad42fce453acb88c
BartoszBartlomiej <bartosz.lewicki@gmail.com>
2019-06-06 14:40:32 +0200

    Updates

---
+++ precache-manifest.f26eb23b3a797fcb5a32de1e5c3c5671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b4e13777aecec707710",
+    "url": "/JustTrip/static/js/runtime~main.3b4e1377.js"
+  },
+  {
+    "revision": "2096928cbbf513c39109",
+    "url": "/JustTrip/static/js/main.2096928c.chunk.js"
+  },
+  {
+    "revision": "5e49819d814d566ad81a",
+    "url": "/JustTrip/static/js/2.5e49819d.chunk.js"
+  },
+  {
+    "revision": "2096928cbbf513c39109",
+    "url": "/JustTrip/static/css/main.4b1ba5ac.chunk.css"
+  },
+  {
+    "revision": "94ee62e064ad0b00a296e853991d05c8",
+    "url": "/JustTrip/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/JustTrip/precache-manifest.f26eb23b3a797fcb5a32de1e5c3c5671.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/JustTrip/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
